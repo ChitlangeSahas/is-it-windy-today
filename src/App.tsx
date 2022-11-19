1c52442dcbf5ce8af3fc862b6cb1361f3a2817c1
ChitlangeSahas <chitlangesahas@gmail.com>
2022-11-18 17:48:00 -0700

    Commiting everything, made it work and look nice

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,83 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import { CssVarsProvider } from '@mui/joy/styles';
 import Box from '@mui/joy/Box';
-import Typography from '@mui/joy/Typography';
+import axios from "axios";
+import ForecastMainTile from "./ForecastMainTile";
+import {IconButton} from "@mui/joy";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+export type Forecast = {
+    shortForecast: string
+    number: number
+    temperature: number
+    temperatureUnit: string
+    windSpeed: string
+    windDirection: string
+    startTime: string
+    icon: string
+}
 
 export default function App() {
+
+    const [dataHasLoaded, setDataHasLoaded] = useState(false)
+    const getForcastData = () => {
+        axios.get("https://api.weather.gov/gridpoints/LWX/89,70/forecast/hourly").then(response => {
+            setForcastsByHour(response.data.properties.periods)
+            setDataHasLoaded(true)
+        })
+    }
+    useEffect(() => {
+        getForcastData()
+    }, [])
+
+    // +1 h = index 0
+    const [hourToDisplay, setHourToDisplay] = useState(0)
+    const [forecastByHour, setForcastsByHour] = useState<Forecast[]>([])
+
+    const incrementHour = () => {
+        setHourToDisplay(hourToDisplay + 1)
+    }
+    const decrementHour = () => {
+        if (hourToDisplay < 1) return
+        setHourToDisplay(hourToDisplay - 1)
+    }
+
+    const getTemperatureTrendData = () => {
+        // Probably best to not tie data structure to the chart needs, but it's okay for now :)
+       return forecastByHour.slice(0,23).map(forecast => {
+           return {
+               name: `${forecast.number}h`,
+               temp: forecast.temperature
+           }
+       })
+    }
+
+    const getWindTrendData = () => {
+        return forecastByHour.map(forecast => {
+            return {
+                name: `${forecast.number}h`,
+                windSpeed: forecast.windSpeed,
+                windDirection: forecast.windDirection
+            }
+        })
+    }
+
+
     return (
         <CssVarsProvider>
-            <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-                <Typography level={"h1"}>Hi 👋</Typography>
+            <Box sx={{display: 'flex',flexDirection: 'column', alignItems: 'center', alignContent: 'center', height: '100vh'}}>
+                <div className={"container"}>
+                    <div className="back-button" >
+                        <IconButton onClick={decrementHour} variant={"solid"}><ArrowBackIcon/></IconButton>
+                    </div>
+                    {dataHasLoaded && <ForecastMainTile forecast={forecastByHour[hourToDisplay]} oneDayTempTrendData={getTemperatureTrendData()} oneDayWindTrendData={getWindTrendData()}/>}
+
+                    <div className="forward-button">
+                        <IconButton onClick={incrementHour} variant={"solid"}><ArrowForwardIcon/></IconButton>
+                    </div>
+                </div>
             </Box>
         </CssVarsProvider>
     );
---
+++ src/ForecastMainTile.tsx
@@ -0,0 +1,133 @@
+import {Forecast} from "./App";
+import React, {useState} from "react";
+import {Button, Card, Divider, Link} from "@mui/joy";
+import Typography from "@mui/joy/Typography";
+import CompassDirection, {Direction} from "./CompassDirection";
+import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
+import {Icon} from "@mui/material";
+import {WiDirectionDown, WiDirectionUp} from "react-icons/wi";
+
+type ForecastMainTileProps = {
+    forecast: Forecast
+    oneDayTempTrendData: any
+    oneDayWindTrendData: any
+}
+
+const ForecastMainTile = (props: ForecastMainTileProps) => {
+    const {forecast, oneDayWindTrendData, oneDayTempTrendData} = props
+    const [displayTwentyFourHourWind, setDisplayTwentyFourHourWind] = useState(false)
+
+
+    const temperatures = oneDayTempTrendData.map((trend: any) => trend.temp)
+
+    const deltaTemperatures = temperatures.map((temp: number, index: number, self: []) => {
+        if (index === 0) {
+            return 0
+        }
+
+        return temp - self[index - 1]
+    })
+
+    console.log(deltaTemperatures)
+    return <>
+        <Card variant={"outlined"}>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <div>
+                    <Typography fontSize={"lg"} fontWeight={"bold"}>
+                        {new Date(forecast.startTime).toLocaleString()}
+                    </Typography>
+                    <Typography fontSize="medium">
+                        {forecast.shortForecast}
+                    </Typography>
+                </div>
+                <div>
+                    <Typography level="h2">
+                        <span>{forecast.temperature}{'° '}{forecast.temperatureUnit}{' '}</span>
+                        {deltaTemperatures[forecast.number - 1] !== 0 &&
+                            <div style={{display: 'inline-flex', flexDirection: 'column', alignItems: 'center'}}>
+                                <Typography fontSize={"medium"}
+                                            color={"info"}><span>{deltaTemperatures[forecast.number - 1]}</span></Typography>
+                            </div>}
+                    </Typography>
+
+                </div>
+
+            </div>
+
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                textAlign: 'center',
+                marginTop: '20px'
+            }}>
+                <div>
+                    <CompassDirection direction={forecast.windDirection as Direction}/>
+                    <Typography level="h2">
+                        {forecast.windDirection}
+                    </Typography>
+                </div>
+                <Typography level="h4">
+                    {forecast.windSpeed}
+                </Typography>
+            </div>
+
+            <Divider sx={{mt: 10}}/>
+            {displayTwentyFourHourWind ?
+                <Typography sx={{mt: 3}} fontSize={"lg"}>Wind trend (24h)</Typography> :
+                <Typography sx={{mt: 3}} fontSize={"lg"}>Wind trend (12h)</Typography>
+            }
+
+
+            <div>
+                {oneDayWindTrendData.slice(0, 11).map((trend: any) => {
+                    return <div style={{display: 'inline-flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <CompassDirection direction={trend.windDirection}/>
+                        <Typography color={"info"}>{trend.windSpeed}</Typography>
+                    </div>
+                })
+                }
+
+            </div>
+            {!displayTwentyFourHourWind &&
+                <Link onClick={() => setDisplayTwentyFourHourWind(true)} sx={{alignSelf: 'flex-end', mt: 2, mr: 1}}>View
+                    more</Link>}
+            {displayTwentyFourHourWind &&
+                <div>
+                    {oneDayWindTrendData.slice(0, 11).map((trend: any) => {
+                        return <div style={{display: 'inline-flex', flexDirection: 'column', alignItems: 'center'}}>
+                            <CompassDirection direction={trend.windDirection}/>
+                            <Typography color={"info"}>{trend.windSpeed}</Typography>
+                        </div>
+                    })
+                    }
+                </div>
+            }
+
+            <Typography sx={{mt: 3}} fontSize={"lg"}>Temperature trend (24h)</Typography>
+
+            <LineChart
+                width={780}
+                height={200}
+                data={props.oneDayTempTrendData}
+                margin={{
+                    top: 30,
+                    right: 30,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <Tooltip/>
+                <Legend/>
+                <Line type="monotone" dataKey="temp" stroke="green"/>
+            </LineChart>
+
+
+        </Card>
+    </>
+}
+
+
+export default ForecastMainTile
